f8e8a8afe59694734a720d899b5f1c595d798714
Gustavo Freitas <78383471+gustavo-freita@users.noreply.github.com>
2023-01-05 20:41:47 -0300

    Delete popupImagem.js

--- popupImagem.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.image__container img').forEach(image =>{
-    image.onclick = () =>{
-        document.querySelector('.popup__image').style.display = 'block';
-        document.querySelector('.popup__image img').src = image.getAttribute('src');
-    }
-});
-document.querySelector('.popup__image span').onclick = () =>{
-    document.querySelector('.popup__image').style.display = 'none';
-}
